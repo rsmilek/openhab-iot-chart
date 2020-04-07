2e2f2db5c840a77b8336465d42b1b93eb14eaf8c
rsmilek <smilek.radim@gmail.com>
2020-04-07 18:34:23 +0200

    + iotTable: UI improvements

--- src/components/iotTable.jsx
+++ src/components/iotTable.jsx
@@ -24,33 +24,31 @@ export default class IotTable extends Component {
     if (sqlAggregates.length <= 0) return null;
     else {
       return (
-        <div>
-          <div className="container">
-            <table className="table text-sm-left table-hover mt-5">
-              <thead>
-                <tr>
-                  <th scope="col"></th>
-                  <th className="text-right" scope="col">
-                    Avg
-                  </th>
-                  <th className="text-right" scope="col">
-                    Min
-                  </th>
-                  <th className="text-right" scope="col">
-                    Max
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">Outdoor</th>
-                  <td className="text-right">{sqlAggregates[0].avg.toFixed(2)}</td>
-                  <td className="text-right">{sqlAggregates[0].min.toFixed(2)}</td>
-                  <td className="text-right">{sqlAggregates[0].max.toFixed(2)}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+        <div className="container">
+          <table className="table table-bordered table-hover text-sm-left mt-5">
+            <thead className="thead-light">
+              <tr>
+                <th scope="col"></th>
+                <th className="text-right" scope="col">
+                  Avg
+                </th>
+                <th className="text-right" scope="col">
+                  Min
+                </th>
+                <th className="text-right" scope="col">
+                  Max
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Outdoor</th>
+                <td className="text-right">{sqlAggregates[0].avg.toFixed(2)}</td>
+                <td className="text-right">{sqlAggregates[0].min.toFixed(2)}</td>
+                <td className="text-right">{sqlAggregates[0].max.toFixed(2)}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       );
     }
