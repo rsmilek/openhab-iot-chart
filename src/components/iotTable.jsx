229d434dfb6bc41f0defd4f0571a3067efc505a2
rsmilek <smilek.radim@gmail.com>
2020-06-05 10:24:42 +0200

    * refactoring of IotTable component to use styling by class container-iottable instead of table in table

--- src/components/iotTable.jsx
+++ src/components/iotTable.jsx
@@ -14,47 +14,32 @@ export default class IotTable extends Component {
     else {
       return (
         <div className="container">
-          <table className="table table-borderless">
-            <thead>
-              <tr>
-                <th style={{ width: "10%" }}></th>
-                <th style={{ width: "80%" }}></th>
-                <th style={{ width: "10%" }}></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td></td>
-                <td>
-                  <table className="table table-bordered table-sm text-sm-left">
-                    <thead className="thead-light">
-                      <tr>
-                        <th scope="col" style={{ width: "40%" }}></th>
-                        <th className="text-right" scope="col" style={{ width: "20%" }}>
-                          Avg
-                        </th>
-                        <th className="text-right" scope="col" style={{ width: "20%" }}>
-                          Min
-                        </th>
-                        <th className="text-right" scope="col" style={{ width: "20%" }}>
-                          Max
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row">Outdoor</th>
-                        <td className="text-right">{sqlAggregate.avg.toFixed(2)}</td>
-                        <td className="text-right">{sqlAggregate.min.toFixed(2)}</td>
-                        <td className="text-right">{sqlAggregate.max.toFixed(2)}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </td>
-                <td></td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="container-iottable">
+            <table className="table table-bordered table-sm text-sm-left">
+              <thead className="thead-light">
+                <tr>
+                  <th scope="col" style={{ width: "40%" }}></th>
+                  <th className="text-right" scope="col" style={{ width: "20%" }}>
+                    Avg
+                  </th>
+                  <th className="text-right" scope="col" style={{ width: "20%" }}>
+                    Min
+                  </th>
+                  <th className="text-right" scope="col" style={{ width: "20%" }}>
+                    Max
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Outdoor</th>
+                  <td className="text-right">{sqlAggregate.avg.toFixed(2)}</td>
+                  <td className="text-right">{sqlAggregate.min.toFixed(2)}</td>
+                  <td className="text-right">{sqlAggregate.max.toFixed(2)}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       );
     }
